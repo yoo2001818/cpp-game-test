775dd9ae3e7ddc6d7c42c719e3c43fe6fc279b91
Duknam Yoo <yoo2001818@gmail.com>
2022-07-08 22:38:18 +0900

    Initialize SDL window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
-#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
 
 int main()
 {
-  std::cout << "Hello, world!" << std::endl;
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_TIMER) != 0) {
+    SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+    return 1;
+  }
+
+  SDL_Window *window;
+
+  window = SDL_CreateWindow("CppGameTest", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, SDL_WINDOW_VULKAN);
+
+  if (window == NULL) {
+    SDL_Log("Unable to create window: %s", SDL_GetError());
+    return 1;
+  }
+
+  SDL_Delay(3000);
+
+  SDL_DestroyWindow(window);
+
+  SDL_Quit();
 
   return 0;
 }
