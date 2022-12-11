e5ea5366df621f409c9893d0f05a2b0e5103b551
Duknam Yoo <yoo2001818@gmail.com>
2022-12-12 02:04:29 +0900

    Add resource manager insert/get function

--- src/resource.hpp
+++ src/resource.hpp
@@ -14,7 +14,9 @@ namespace resource {
     int mUseCount = 0;
     bool mIsLoadable = false;
 
-    resource_entry() {};
+    resource_entry() = default;
+    resource_entry(const resource_entry<T>&) = default;
+    resource_entry(resource_entry<T>&&) = default;
     
     resource_entry(T&& value): mValue(value) {};
     resource_entry(const T& value): mValue(value) {};
@@ -38,6 +40,14 @@ namespace resource {
       this->mHasFailed = false;
     };
 
+    void update(const T& value) {
+      this->mValue = value;
+    };
+
+    void update(const std::function<std::optional<T>()>& load_entry_function) {
+      this->mLoadEntryFunction = load_entry_function;
+    };
+
     std::optional<T>& value() {
       if (!mValue.has_value() && !this->mHasFailed) {
         auto loaded = this->load();
@@ -59,10 +69,43 @@ namespace resource {
   template<class T>
   class resource_manager {
     public:
+    resource_manager() {};
+
+    void insert(std::string name, const T& value) {
+      auto [it, is_created] = this->entries.emplace(name, { value });
+      if (!is_created) {
+        it->second.update(value);
+      }
+    };
+
+    void insert(std::string name, const std::function<std::optional<T>()>& load_entry_function) {
+      auto [it, is_created] = this->entries.emplace(name, { load_entry_function });
+      if (!is_created) {
+        it->second.update(load_entry_function);
+      }
+    };
+
+    resource_entry<T>& get(std::string name) {
+      // Note that this will always give an entry even if the resource is not
+      // to be found. This is done to allow lazy initialization, and track
+      // used-but-not-available resources.
+      // Also note that resource_entry<T> reference is stable - you can call
+      // this function and use it as long as you want.
+      // (Though we will need some kind of reference count tracking later)
+
+      auto it = this->entries.find(name);
+      if (it == this->entries.end()) {
+        // Create new empty resource
+        auto [new_it, is_created] = this->entries.emplace(name, resource_entry<T>());
+        return new_it->second;
+      }
+      return it->second;
+    }
 
     private:
     std::unordered_map<std::string, resource_entry<T>> entries;
-    std::vector<std::reference_wrapper<resource_entry<T>>> used_entries;
+    // TODO: We don't do any cleanups yet, but we'll need it later.
+    // std::vector<std::reference_wrapper<resource_entry<T>>> used_entries;
   };
 }
 
