0c7787437659d906cf88b9769bd11bb2a46a1062
Duknam Yoo <yoo2001818@gmail.com>
2023-01-07 23:53:12 +0900

    Add animation.hpp

---
+++ src/animation.hpp
@@ -0,0 +1,20 @@
+#ifndef ANIMATION_HPP_
+#define ANIMATION_HPP_
+
+#include <map>
+
+namespace animation {
+  class animation_state {
+    public:
+  };
+
+  class animation_action {
+    public:
+  };
+
+  class animation {
+
+  };
+}
+
+#endif // ANIMATION_HPP_
