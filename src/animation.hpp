0a18facbcf27eb3f8c6826655b4978c7852fe10f
Duknam Yoo <yoo2001818@gmail.com>
2023-01-08 23:58:37 +0900

    Add animation base structs

--- src/animation.hpp
+++ src/animation.hpp
@@ -2,18 +2,34 @@
 #define ANIMATION_HPP_
 
 #include <map>
+#include <list>
+#include <string>
 
 namespace animation {
   class animation_state {
     public:
+    std::string name;
+    std::map<std::string, std::string> actions;
+    float duration;
   };
 
   class animation_action {
     public:
+    std::string name;
+  };
+
+  class animation_definition {
+    public:
+    std::string initial_state;
+    std::map<std::string, animation_state> states;
+    std::map<std::string, animation_action> actions;
   };
 
   class animation {
-
+    public:
+    animation_state& state;
+    float time;
+    animation_definition& definition;
   };
 }
 
