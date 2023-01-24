9e786c8a2c5213b3c84b43b3643192f42bc9e4f6
Duknam Yoo <yoo2001818@gmail.com>
2023-01-24 23:34:12 +0900

    Add animation cpp

---
+++ src/animation.cpp
@@ -0,0 +1,21 @@
+#include "animation.hpp"
+
+void animation::animation_state::addEdge(std::string actionName, std::string nextState, bool isInterruptable) {
+  
+}
+
+void animation::animation_state::removeEdge(std::string actionName) {
+  
+}
+
+void animation::animation_definition::addState(animation_state state) {
+
+}
+
+void animation::animation_definition::removeState(std::string name) {
+
+}
+
+void animation::updateAnimation(game& game) {
+  
+}
