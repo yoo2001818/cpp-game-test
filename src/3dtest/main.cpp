1fd8d6be476f36bde8e683747780d8ba97e00b58
Duknam Yoo <yoo2001818@gmail.com>
2023-09-04 23:59:40 +0900

    Add GL triangle

--- src/3dtest/main.cpp
+++ src/3dtest/main.cpp
@@ -34,8 +34,75 @@ int main() {
     return 1;
   }
 
+  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+  float colors[] = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+  unsigned int indices[] = {0, 1, 2};
+
   unsigned int vbo;
   glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  unsigned int vbo2;
+  glGenBuffers(1, &vbo2);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo2);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+  unsigned int ebo;
+  glGenBuffers(1, &ebo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  unsigned int vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo2);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(1);
+
+  const char *vertexShaderSource =
+      "#version 330 core\n"
+      "layout (location = 0) in vec3 vPos;\n"
+      "layout (location = 1) in vec3 vColor;\n"
+      "out vec3 aColor;\n"
+      "void main()\n"
+      "{\n"
+      "   aColor = vColor;\n"
+      "   gl_Position = vec4(vPos.x, vPos.y, vPos.z, 1.0);\n"
+      "}\0";
+
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  const char *fragmentShaderSource = "#version 330 core\n"
+                                     "in vec3 aColor;\n"
+                                     "out vec4 FragColor;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "    FragColor = vec4(aColor, 1.0f);\n"
+                                     "}\0";
+
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
 
   {
     while (true) {
@@ -53,13 +120,10 @@ int main() {
       glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
       glClear(GL_COLOR_BUFFER_BIT);
 
-      glColor4f(0.0f, 0.0f, 0.0f, 1.0f);
-      glBegin(GL_QUADS);
-      glVertex2f(-0.5f, -0.5f);
-      glVertex2f(0.5f, -0.5f);
-      glVertex2f(0.5f, 0.5f);
-      glVertex2f(-0.5f, 0.5f);
-      glEnd();
+      glUseProgram(shaderProgram);
+      glBindVertexArray(vao);
+      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+      glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
       SDL_GL_SwapWindow(window);
 
