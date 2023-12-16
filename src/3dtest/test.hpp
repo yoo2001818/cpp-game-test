bb69f47ef5a22ff996d8cd8fa13a7011a455f3b8
Duknam Yoo <yoo2001818@gmail.com>
2023-12-16 23:58:13 +0900

    Separate material and shader

--- src/3dtest/test.hpp
+++ src/3dtest/test.hpp
@@ -29,11 +29,19 @@ struct light_block {
   glm::vec3 mPosition;
   glm::vec3 mColor;
 };
-class material {
+class material_shader {
 public:
   unsigned int mVertexShaderId = -1;
   unsigned int mFragmentShaderId = -1;
   unsigned int mProgramId = -1;
+  std::string mVertexShader;
+  std::string mFragmentShader;
+  void prepare();
+  void dispose();
+};
+class material {
+public:
+  std::map<int, material_shader> mShaders;
   void prepare(const std::vector<light_block> &pLights);
   void dispose();
 };
